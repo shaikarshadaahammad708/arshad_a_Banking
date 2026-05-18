2065320e0beb5a58cbcfcba319481d1d003fe8ec
shaikarshadaahammad708 <shaikarshadaahammad708@gmail.com>
2026-05-18 12:16:59 +0000

    Added

--- Test/src/Even.java
+++ Test/src/Even.java
@@ -1,4 +1,12 @@
 
 public class Even {
+public static void main(String[] args) {
+	int number = 10; // You can change this number to test with different values
 
+	if (number % 2 == 0) {
+		System.out.println(number + " is an even number.");
+	} else {
+		System.out.println(number + " is not an even number.");
+	}
+}
 }
