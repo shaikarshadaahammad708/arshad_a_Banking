0ee80ee1cea935859725a599519b8d6dcecfab7d
shaikarshadaahammad708 <shaikarshadaahammad708@gmail.com>
2026-05-18 12:06:29 +0000

    added

--- Bank/src/com/bb/controllor/Test.java
+++
@@ -1,7 +0,0 @@
-package com.bb.controllor;
-
-public class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello World!");
-	}
-}
---
+++ Test/src/com/bb/controller/Ram.java
@@ -0,0 +1,7 @@
+package com.bb.controller;
+
+public class Ram {
+public static void main(String[] args) {
+	System.out.println("Ram");
+}
+}
