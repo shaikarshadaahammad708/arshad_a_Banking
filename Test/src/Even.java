0904ba4451b84c23e49f5f1d50114e5ea9de6b4e
shaikarshadaahammad708 <shaikarshadaahammad708@gmail.com>
2026-05-19 04:32:51 +0000

    code

--- Test/src/Even.java
+++ Test/src/Even.java
@@ -12,6 +12,8 @@ public static void main(String[] args) {
 		System.out.println("Try again with an even number.");
 		System.out.println("Remember, even numbers are divisible by 2.");
 		System.out.println("Good luck!");
-	}{System.out.println("This is the end of the program.");}
+	}{System.out.println("This is the end of the program.");}{System.out.println("Thank you for using the Even number checker.");}
+	System.out.println("Have a great day!");
+	System.out.println("Goodbye!");
 }
 }
